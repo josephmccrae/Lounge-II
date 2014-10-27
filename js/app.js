41b69c43d73ecc66eb4691c66b6b470f9e4e1941
Joseph <josevem78@gmail.com>
2014-10-26 22:47:39 -0400

    upload13

--- js/app.js
+++ js/app.js
@@ -23,15 +23,12 @@ $(document).ready(function(){
     var getInfo = function(){
 
         var result = $.ajax({
-            url: "http://www.comicvine.com/api/characters/?api_key=145adb79c062d3d1ce533699ca10282a963deede",
+            url: "http://www.comicvine.com/api/characters/?api_key=145adb79c062d3d1ce533699ca10282a963deede&filter=name:Batman",
             dataType: "jsonp",
             type: "GET",
             })
-        
-        .done(function(result){
-		console.log(result);
-		});
-	   };
+        });
+
 
 
 
