fcabbfbc4aba20e3ed58dd4f299527732a76af50
Joseph <josevem78@gmail.com>
2014-10-26 23:06:02 -0400

    upload17

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
     
     $('.heroSelect').on('click',function(){
-        getInfo();
+        getInfo(name);
     });
 });
 
-    
+    var showHero = function(characters) {
+	
+	// clone our result template code
+	var result = $('.hidden .hero').clone();
+	
+	// set some properties related to asker
+	var hero = result.find('.name');
+        
+	return result;
+};
     
 
-    var getInfo = function(characters){
+    var getInfo = function(name){
 
         var result = $.ajax({
             url: "http://www.comicvine.com/api/characters/?api_key=145adb79c062d3d1ce533699ca10282a963deede&filter=name:Batman",
@@ -17,7 +26,7 @@ $(document).ready(function(){
             })
         
         .done(function(result){
-            console.log(characters.name);
+           $('.results').append(answerer);
         });
     };
             
