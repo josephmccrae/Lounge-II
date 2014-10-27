377b68f434b194871529a4470f39f8bf215f8492
Joseph <josevem78@gmail.com>
2014-10-26 22:40:15 -0400

    upload9

--- js/app.js
+++ js/app.js
@@ -6,6 +6,19 @@ $(document).ready(function(){
 });
 
     
+    var showHero = function(characters) {
+	
+        // clone our result template code
+        var result = $('.hidden .hero').clone();
+
+        // Set the question properties in result
+        var heroElem = result.find('.name');
+        heroElem.text(characters.name);
+
+        return result;
+    };
+
+
 
     var getInfo = function(){
 
@@ -13,7 +26,15 @@ $(document).ready(function(){
             url: "http://www.comicvine.com/api/characters/?api_key=145adb79c062d3d1ce533699ca10282a963deede",
             dataType: "jsonp",
             type: "GET",
-            });        
+            });
+        
+        
+        .done(function(result){
+		$.each(result.items, function(i, item) {
+			var final = showHero(item);
+			$('.results').append(final);
+		});
+	   })
     };
 
 
